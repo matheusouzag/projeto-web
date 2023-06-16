9ab85b9859949b0f8bc65be0e52fce50cbeb330a
Matheus Souza <mathsouzag@gmail.com>
2023-06-16 15:48:27 -0300

    Header pronto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
 export default function Home() {
-    return <h1>Gamebald</h1>;
+    return (
+        <body className="bg-preto">
+            <header className="bg-amarelo h-18">
+                <div className="flex flex-row justify-around items-center">
+                    <picture className="w-10">
+                        <img src="img/gamebald.png" alt="Logo do G" />
+                    </picture>
+
+                    <div className="flex justify-center">
+                        <button className="border-2 px-5 py-4 h-5 flex items-center border-black mr-6">
+                            Login
+                        </button>
+                        <button className="border-2 border-preto bg-preto text-branco px-5 py-4 flex items-center h-5">
+                            Cadastro
+                        </button>
+                    </div>
+                </div>
+            </header>
+        </body>
+    );
 }
